dc653a35326a6af03f3a6e8c29398372113b9137
kevin <righttrianglesrkewl3@gmail.com>
2021-08-09 16:18:20 -0700

    more clean

--- app/main/util/dto.py
+++ app/main/util/dto.py
@@ -1,16 +1,5 @@
 from flask_restx import Namespace, fields
 
-
-# class UserDto:
-#     api = Namespace('user', description='user related operations')
-#     user = api.model('user', {
-#         'email': fields.String(required=True, description='user email address'),
-#         'username': fields.String(required=True, description='user username'),
-#         'password': fields.String(required=True, description='user password'),
-#         'public_id': fields.String(description='user Identifier')
-#     })
-
-
 class PeakDto:
     api = Namespace('peak', description='peak related operations')
     peak = api.model('peak', {
