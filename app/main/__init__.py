97e0e52f2c0bad06c18e12ee08a82e21b6a64a6d
kevin <righttrianglesrkewl3@gmail.com>
2021-08-10 13:41:20 -0700

    update

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -11,6 +11,7 @@ flask_bcrypt = Bcrypt()
 
 def create_app(config_name: str) -> Flask:
     app = Flask(__name__)
+    app.config.SWAGGER_UI_DOC_EXPANSION = 'list'
     app.config.from_object(config_by_name[config_name])
     db.init_app(app)
     flask_bcrypt.init_app(app)
