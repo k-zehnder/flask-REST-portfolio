2acf50e973dc23b485ab84ef0a98655dd7cb7b94
kevin <righttrianglesrkewl3@gmail.com>
2021-08-12 08:57:38 -0700

    added tests

--- app/test/test_api.py
+++ app/test/test_api.py
@@ -20,11 +20,35 @@ class FlaskTestCase(TestCase):
         db.session.remove()
         db.drop_all()
 
+    # test databases
     def test_database(self):
         tester = self.app.test_client(self)
         response = tester.get('/', content_type='application/json')
         self.assertEqual(response.status_code, 200)
 
+    # test index
+    def test_index(self):
+        tester = self.app.test_client(self)
+        response = tester.get('/', content_type='application/json')
+        self.assertEqual(response.status_code, 200)
+
+    # test peaks
+    def test_peaks(self):
+        tester = self.app.test_client(self)
+        response = tester.get('/', content_type='application/json')
+        self.assertEqual(response.status_code, 200)
+
+    # test reviews
+    def test_index(self):
+        tester = self.app.test_client(self)
+        response = tester.get('/', content_type='application/json')
+        self.assertEqual(response.status_code, 200)
+
+    # test photo
+    def test_photo(self):
+        tester = self.app.test_client(self)
+        response = tester.get('/', content_type='application/json')
+        self.assertEqual(response.status_code, 200)
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
